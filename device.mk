42260610715e716726528b2f19e3d7ff5e98483b
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    OP4AD9: Build dummy android.hidl.[base,manager]@1.0
    [daniml3]: convert to blueprint
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vendor_overlay/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Overlays-rro
 PRODUCT_PACKAGES += \
     OPPODialerOverlay
