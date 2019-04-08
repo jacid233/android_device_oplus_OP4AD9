cdfd72f08d8c7a6f21892e4e285c47b8d96592f7
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    OP4AD9: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, vendor/oppo/OP4AD9/OP4AD9-vendor.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
