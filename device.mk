613f8de0022fe74cf81aba1a7ad362f24656d5a3
jacid233 <jacid11@outlook.com>
2023-04-19 07:55:45 +0800

    OP4AD9: use dynamic partitions and not build super partition

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
 # Recovery init
 PRODUCT_PACKAGES += \
 	init.recovery.qcom.rc
