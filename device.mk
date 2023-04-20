4e22db57c4d9603785806cb07ca1cd578717765b
jacid233 <jacid11@outlook.com>
2023-04-20 17:42:57 +0800

    OP4AD9: overlay: use overlay-lineage

--- device.mk
+++ device.mk
@@ -62,7 +62,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
