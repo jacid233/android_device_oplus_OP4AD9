0fc802712336b9199b5ec4886c0f694aaea9286d
jacid233 <jacid11@outlook.com>
2023-04-26 09:57:10 +0800

    OP4AD9: blob: extracted from C22

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,9 @@ function blob_fixup() {
     odm/lib64/libgf_hal_G5.so)
         sed -i "s/ro.boot.flash.locked/vendor.flash.locked\x00/" "${2}"
     ;;
+    odm/lib64/libgf_hal_G6.so)
+        sed -i "s/ro.boot.flash.locked/vendor.flash.locked\x00/" "${2}"
+    ;;
     esac
 }
 
