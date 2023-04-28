9bed3d998462960460a2b0a6ff61a6fe2d89461e
jacid233 <jacid11@outlook.com>
2023-04-28 21:24:54 +0800

    OP4AD9: fod: update fingerprint inscreen sensor location

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -29,8 +29,8 @@
 #define PANEL_BLANK_PATH "/sys/kernel/oppo_display/notify_panel_blank"
 #define AOD_LIGHT_MODE_PATH "/sys/kernel/oppo_display/aod_light_mode_set"
 #define DIMLAYER_PATH "/sys/kernel/oppo_display/dimlayer_hbm"
-#define X_POS 445
-#define Y_POS 1795
+#define X_POS 450
+#define Y_POS 1805
 #define FP_SIZE 190
 #define ON 1
 #define OFF 0
