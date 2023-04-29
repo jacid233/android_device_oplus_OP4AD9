91a4db77fa35d5957c21094d31994d30f865d78c
LuK1337 <priv.luk@gmail.com>
2023-04-29 18:19:32 +0800

    OP4AD9: Import sensors/2.0/multihal
     * autobrightness

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensor
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.multihal.oplus_kona \
     libsensorndkbridge
 
 # Telephony
