72e80f67d35787d6e44da3d0140ad1b5b27e6fc8
jacid233 <jacid11@outlook.com>
2023-06-04 03:27:09 +0800

    OP4AD9: arrow13.0 bringup

--- arrow_OP4AD9.mk
+++ arrow_OP4AD9.mk
@@ -18,8 +18,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+ARROW_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
@@ -27,7 +28,7 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_OP4AD9
+PRODUCT_NAME := arrow_OP4AD9
 PRODUCT_DEVICE := OP4AD9
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := PDHM00
