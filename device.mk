367334c7b356164cde5edae571abd08da1f7dc33
jacid233 <jacid11@outlook.com>
2023-06-14 15:33:00 +0800

    OP4AD9: Configure OPlusExtras

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    OPlusExtrasResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
