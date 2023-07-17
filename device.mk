5ae172b6ff7bb4bd8b5c32a3f084a804df651656
jacid233 <jacid11@outlook.com>
2023-07-17 09:48:03 +0800

    OP4AD9: set shipping api to 29

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, device/oplus/sm8250-common/common.mk)
 $(call inherit-product, vendor/oplus/OP4AD9/OP4AD9-vendor.mk)
 
 # API
-PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Audio
 TARGET_HAS_AUDIO_LVIMFS := true
